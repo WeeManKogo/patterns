045a0b55f283ca57453ae71ba149b057b6904a6f
Lukasz Gos <lukasz@nitro-digital.com>
2019-04-17 18:50:16 +0200

    Factory: updated tests and README file

---
+++ tests/CreationalDesign/Factory/FactoryTest.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace TestPatterns\CreationalDesign\Singleton;
+
+
+use Patterns\CreationalDesign\Factory\{
+  Circle,
+  Square,
+  ShapeFactory,
+};
+use PHPUnit\Framework\TestCase;
+
+class FactoryTest extends TestCase
+{
+  public function testTypeOfShapeShouldBeCircle()
+  {
+    $shapeFactory = new ShapeFactory();
+
+    $this->assertInstanceOf(
+      Circle::class,
+      $shapeFactory->getShape(ShapeFactory::$SHAPE_CIRCLE)
+    );
+  }
+
+  public function testTypeOfShapeShouldBeSquare()
+  {
+    $shapeFactory = new ShapeFactory();
+
+    $this->assertInstanceOf(
+      Square::class,
+      $shapeFactory->getShape(ShapeFactory::$SHAPE_SQUARE)
+    );
+  }
+
+}
